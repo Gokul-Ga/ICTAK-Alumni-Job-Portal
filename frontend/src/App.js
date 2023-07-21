21dd93029e15b211b45f0d18bcffd93f7d079523
Libin87 <91073300+Libin87@users.noreply.github.com>
2023-07-21 23:54:27 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-// import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "../src/App.css";
+
+import SignupUser from "./components/USER/SignupUser";
+import LoginNavbarUser from "./components/USER/LoginNavbarUser";
+import ViewJobsUser from "./components/USER/ViewJobsUser";
+import FooterUser from "./components/USER/FooterUser";
+import LoginUser from "./components/USER/LoginUser";
+import SignupNavbarUser from "./components/USER/SignupNavbarUser";
 function App() {
   return (
     <div className="App">
-      <h1>ICTAK Alumni Job Portal</h1>
+     <Routes>
+      <Route path="/SignupNavbarUser" element={<SignupNavbarUser/>}></Route>
+      <Route path="/LoginUser" element={<LoginUser/>}></Route>
+        <Route path="/ViewJobsUser" element={<ViewJobsUser/>}></Route>
+        <Route path="/SignupUser" element={<SignupUser/>}></Route>
+        <Route path="/LoginNavbarUser" element={<LoginNavbarUser/>}></Route>
+        <Route path="/FooterUser" element={<FooterUser/>}></Route>
+      </Routes>
     </div>
   );
 }
